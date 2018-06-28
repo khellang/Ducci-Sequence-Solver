b9674dcba641b49cc270515566d05ab7516eec1f
Owen Carpenter <owenc343@gmail.com>
2018-06-28 10:08:13 -0400

    Trimmed down solution

--- venv/DucciSolver2.py
+++ venv/DucciSolver2.py
@@ -1,17 +1,12 @@
 def check_end(ducci):
-    for i in ducci:
-        if i != 0:
-            return False
-    return  True
+    return all(i == 0 for i in ducci)
 
 def compute(ducci):
     copy = ducci[0]
     new_seq = ducci
-    for i in range(0,len(ducci)):
-        if i != len(ducci)-1:
-            new_seq[i] = abs(ducci[i] - ducci[i+1])
-        else:
-            new_seq[i] = abs(ducci[i] - copy)
+    for i in range(0,len(ducci)-1):
+        new_seq[i] = abs(ducci[i] - ducci[i+1])
+    new_seq[len(ducci)-1] = abs(ducci[len(ducci)-1] - copy)
     return tuple(new_seq)
 
 def solve(ducci):
@@ -26,11 +21,11 @@ def solve(ducci):
             steps += 1
     return steps
 
-# ducci1 = [1,5,7,9,9]
-# ducci2 = [1,2,1,2,1,0]
-# ducci3 = (10,12,41,62,31,50)
-# ducci4 = (10,12,41,62,31)
-#
-# steps = solve(ducci1)
-#
-# print(steps)
+ducci1 = [1,5,7,9,9]
+ducci2 = [1,2,1,2,1,0]
+ducci3 = (10,12,41,62,31,50)
+ducci4 = (10,12,41,62,31)
+
+steps = solve(ducci4)
+
+print(steps)
